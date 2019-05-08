84516e142bc254ad2b3176aa4ac973790e756a3e
Simar <simar.public@gmail.com>
2019-05-08 16:18:19 -0400

    changes

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <div class="title">
+    <div class="title" v-if="pastInitalDelay">
       <h1 v-if="post.title">{{post.title}}</h1>
-      <vue-content-loading v-else
+      <vue-content-loading v-else-if="pastInitalDelay"
         :height="loader.title.height"
         :primary="loader.title.primary"
         :secondary="loader.title.secondary">
@@ -12,7 +12,7 @@
     <div class="metadata">
       <span class="date">
         <timeago v-if="post.date" :since="post.date" :auto-update="10"></timeago>
-        <vue-content-loading v-else
+        <vue-content-loading v-else-if="pastInitalDelay"
         :height="loader.date.height"
         :primary="loader.date.primary"
         :secondary="loader.date.secondary">
@@ -22,7 +22,7 @@
     </div>
     <div class="markdown-container">
       <vue-markdown v-if="post.content" :source="post.content"></vue-markdown>
-      <vue-content-loading v-else
+      <vue-content-loading v-else-if="pastInitalDelay"
         :height="loader.content.height"
         :primary="loader.content.primary"
         :secondary="loader.content.secondary">
@@ -48,6 +48,7 @@ export default {
   },
   data () {
     return {
+	  pastInitalDelay: false,
       loader: {
         title: {
           height: 34.5,
@@ -69,41 +70,21 @@ export default {
     }
    },
    mounted () {
-    this.$nextTick(function () {
-      setTimeout(() => {
-        this.$set(this, 'post', {
-          title: 'Lorem Ipsum is not simply random text',
-          date: new Date(),
-          content: `
-  ### Getting Started
-  Lorem Ipsum is simply **dummy text of the printing** and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-  <img src="http://gulfinfotech.com/wp-content/uploads/revslider/rev_slider_example/placeholder-blue.png" width="100%" />
-
-  ### More Information
-
-  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-  The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-
-  ### Last Step
-
-  Nullam orci lacus, auctor eget maximus ac, eleifend suscipit mi. Vivamus id sagittis ipsum. In a ex mattis velit ultricies bibendum. Ut vel sodales turpis, eget dapibus lorem. Integer ut condimentum orci. Nulla luctus leo eget orci aliquet, non hendrerit justo vulputate. Proin ipsum nisi, tincidunt a tortor in, semper euismod nisl. Maecenas nisl nunc, finibus non risus ut, fermentum accumsan arcu. Sed posuere mi vitae augue consectetur, vitae rhoncus libero lobortis. Quisque sed metus metus. Morbi sit amet felis odio. Nullam posuere nisl a purus dapibus convallis. Duis tincidunt erat in sagittis iaculis. Duis lacus ex, tempus sit amet lectus sit amet, sollicitudin porttitor eros. Quisque ullamcorper sollicitudin urna nec suscipit.
-
-  \`\`\` javascript
-  var foo = function (bar) {
-    return bar++;
-  };
-
-  console.log(foo(5));
-  \`\`\`
-
-  Nam eu velit ac quam sollicitudin malesuada nec a nisi. Vestibulum gravida leo in odio vehicula maximus. Donec nisl mauris, sollicitudin sit amet massa ac, efficitur dapibus mauris. Aenean aliquet diam quis turpis dapibus, facilisis pellentesque est ullamcorper. Sed efficitur imperdiet justo ac porta. Suspendisse nec eros nec urna molestie molestie. Donec dictum dui sed nulla blandit gravida. In hac habitasse platea dictumst. Ut iaculis sapien vitae magna congue, nec egestas mi ullamcorper. Maecenas viverra eu ligula ac posuere. Quisque neque magna, venenatis non nisl eu, suscipit condimentum lacus. Sed in est eget dui gravida consequat sed in arcu. In vitae est justo. Nullam viverra viverra mi aliquam iaculis.
-        `
-        }
-      );
-      }, 2000);
-    })
+	setTimeout(() => {
+		this.$set(this, 'pastInitalDelay', true);
+	}, 1000)
+	fetch("https://raw.githubusercontent.com/iSimar/Posts/master/test.md")
+	.then((res) => {
+		res.text()
+		.then((text) => {
+			this.$set(this, 'pastInitalDelay', true);
+			this.$set(this, 'post', {
+				title: 'Lorem Ipsum is not simply random text',
+				date: new Date(),
+				content: text
+			})
+		})
+	})
   }
 }
 </script>
@@ -116,6 +97,7 @@ export default {
     padding-right: 30px;
     padding-top: 20px;
     overflow-y: scroll;
+	padding-bottom: 100px;
   }
   @media (max-width: 2024px) {
     .container {
